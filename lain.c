d02507259f278abfd8a586d27739751622813060
FF0000-R <poupidoupidou@gmail.com>
2022-01-24 03:21:27 +0000

    working?

---
+++ lain.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <curl/curl.h>
+#include <string.h>
+
+int download( char *gen ) {
+	CURL *curl;
+	FILE *fp;
+	int result;
+	char url[] = {"https://pastebin.com/raw/"};
+
+	fp = fopen(gen, "wb");
+	strcat(url, gen);
+
+	curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+	curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+
+	result = curl_easy_perform(curl);
+
+	if(result == CURLE_OK)
+		printf("dl success\n");
+	else {
+		printf("dl ERROR: %s\n", curl_easy_strerror(result));
+		remove(gen);
+	}
+
+	fclose(fp);
+	curl_easy_cleanup(curl);
+}
+
+
+int main(){
+
+	int i, j, k, l, m, n, o, p;
+	char ref[] = {"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char url[] = {"00000000"};
+	int base = sizeof ref - 1;
+
+	for (i = 0 ; i < base ; i++){
+		for (j = 0 ; j < base ; j++){
+			for (k = 0 ; k < base ; k++){
+				for (l = 0 ; l < base ; l++){
+					for (m = 0 ; m < base ; m++){
+						for (n = 0 ; n < base ; n++){
+							for (o = 0 ; o < base ; o++){
+								for (p = 0 ; p < base ; p++){
+								 
+									url[7] = ref[p];
+									printf("%s\n", url);
+									download(url);
+									}
+								url[6] = ref[o];
+								}
+							url[5] = ref[n];
+							}
+						url[4] = ref[m];
+						}
+					url[3] = ref[l];
+					}
+				url[2] = ref[k];
+				}
+
+			url[1] = ref[j];
+		}
+		url[0] = ref[i];
+		printf("\n");
+	}
+	return 0;
+}
